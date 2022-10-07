89f06d639646162318eef9deed46a3071d34a594
Abdullah Nadeem <azn5413@e5-cse-trantor01.cse.psu.edu>
2022-10-07 12:03:34 -0400

    Edited

--- mrhw/q1/efficientflights.py
+++ mrhw/q1/efficientflights.py
@@ -1,1 +1,24 @@
-def effiecint(x,)
+from mrjob.job import MRJob
+
+class Flight(MRJob):
+    
+    def mapper(self,key,line):
+	parts = line.split("\t")
+	origin_airport = parts[0]
+	destination_airport = parts[1}
+	passengers = parts[2]
+		
+	key = origin_airport
+	value = (passenegers, "arrive")
+	yield(key, value)
+
+	key = destination_airport
+	value = (passenegers, "leave")
+	yield(key, value)
+
+    def reducer(self, key, values):
+	yield (key, (x,y))
+
+if __name__ == '__main__':
+    Flight.run()
+
