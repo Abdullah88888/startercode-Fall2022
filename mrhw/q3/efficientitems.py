42846515875c478e7f56ffd0eaa90967130d6641
Abdullah <azn5413@psu.edu>
2022-10-07 22:16:16 -0400

    Edited

---
+++ mrhw/q3/efficientitems.py
@@ -0,0 +1,16 @@
+from mrjob.job import MRJob   # MRJob version
+
+class Orders0(MRJob):  #MRJob version
+    def mapper(self, key, line):
+        parts = line.split("\t")
+        stock_code = float(parts[0])
+        amount = float(parts[3])
+        numcountries = parts[5]
+	mostpopular = parts[7]
+        yield (stock_code, amount, numcountries, mostpopular)
+
+    def reducer(self, key, values):
+        yield (key, values)
+
+if __name__ == '__main__':
+    Efficientitems.run()
